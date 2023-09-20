092b985732fd3eb3a57e56c03afa024ef17cc012
John Doe <johndoe@email.com>
2023-09-20 05:48:31 +0530

    first commit

---
+++ trigo calc.py
@@ -0,0 +1,9 @@
+import math
+A = float(input("Enter S "))
+B=float(input("Enter L "))
+C=(A**2+B**2)**0.5
+tan=B/A
+sin=B/C
+print(tan)
+print(math.degrees(math.acos((A**2+C**2-B**2)/(2*A*C))))
+print(sin)
